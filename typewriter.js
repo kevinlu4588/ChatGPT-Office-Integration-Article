b3747722c01b87a248b8d908cf5410550c1c7478
kevinlu4588 <kevinlu4588@gmail.com>
2023-04-14 15:18:56 -0400

    Formatting

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,12 @@
 var j = 0;
 var txt1 = "How Can I Help?";
 var txt2 = "How Can I Hurt?";
-var speed = 100; /* The speed/duration of the effect in milliseconds */
+var speed1 = 100; /* The speed1/duration of the effect in milliseconds */
 function articleIntro() {
     if (j < txt1.length) {
         document.getElementById("intro-title").innerHTML += txt1.charAt(j);
       j++;
-      setTimeout(articleIntro, speed);
+      setTimeout(articleIntro, speed1);
     }
     else{
         setTimeout(deleteIntro, 1000);
@@ -18,10 +18,10 @@ function articleIntro() {
 
         document.getElementById("intro-title").innerHTML = txt1.substring(0, j);
         j--;
-        setTimeout(deleteIntro, speed);
+        setTimeout(deleteIntro, speed1);
     }
     else{
-        setTimeout(articleIntro2, speed);
+        setTimeout(articleIntro2, speed1);
     }
   }
 
@@ -29,6 +29,6 @@ function articleIntro() {
     if (j < txt2.length) {
         document.getElementById("intro-title").innerHTML += txt2.charAt(j);
       j++;
-      setTimeout(articleIntro2, speed);
+      setTimeout(articleIntro2, speed1);
     }
   }
